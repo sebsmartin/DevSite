d0ef1d4f00ec8349862ce36512d7b90aaf25ef34
SebaAravena <sebastian@izytech.cl>
2020-09-04 18:59:29 -0400

    hola mundo

--- MiSitioWeb/Controllers/HomeController.cs
+++ MiSitioWeb/Controllers/HomeController.cs
@@ -6,24 +6,43 @@ using System.Web.Mvc;
 
 namespace MiSitioWeb.Controllers
 {
+
     public class HomeController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
-
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult Contact(Models.Contacto contacto)
         {
-            ViewBag.Message = "Your application description page.";
+            var db = new Models.ContactoContext();
+            db.Contactos.Add(contacto);
+            try
+            {
+                db.SaveChanges();
+                ViewBag.Message = "Tu mensaje se ha guardado";
+            }
+            catch (Exception)
+            {
+
+                ViewBag.Message = "Error al guardar";
+            }
+            //ViewBag.Message = "Si requieres servicios de desarrollo de software o tienes preguntas , contactame";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Si requieres servicios de desarrollo de software o tienes preguntas , contactame";
 
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
             return View();
         }
     }
---
+++ MiSitioWeb/Models/Contacto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiSitioWeb.Models
+{
+    public class Contacto
+    {
+
+        public string Nombre { get; set; }
+        [Key]
+        public string Email { get; set; }
+        public string Asunto { get; set; }
+        public string Mensaje { get; set; }
+
+
+    }
+
+    public class ContactoContext : DbContext
+    {
+        public DbSet<Contacto> Contactos { get; set; }
+    }
+}
